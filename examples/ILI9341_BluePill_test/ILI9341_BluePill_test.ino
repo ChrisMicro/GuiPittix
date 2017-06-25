2a1af71a067ccaaeefbec4f8d2bea762cb431e0f
ChrisMicro <c.hab@gmx.net>
2017-06-25 07:28:21 +0200

    GPL3 moved to github

--- examples/ILI9341_BluePill_test/ILI9341_BluePill_test.ino
+++ examples/ILI9341_BluePill_test/ILI9341_BluePill_test.ino
@@ -4,6 +4,8 @@
  * Hardware:
  *  TFT: ILI9341 with SPI
  *  MCU: STM32F103 BluePill
+ *
+ * 2017, ChrisMicro
  * 
  */
 #include "SPI.h"
@@ -143,39 +145,3 @@ void loop(void)
   Led2.on();
   delay(1000);
 }
-
-
-
-
-
-
-
-
-
-
-
-/* GuiPittix simple graphical user interface elemetns
- *   
- * © ChrisMicro 2017.
- *
- * This file is part of GuiPittix.
- *
- * GuiPittix is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * GuiPittix is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with simple_GUI_elements.  If not, see <http://www.gnu.org/licenses/>.
- *
- * If you use simple_GUI_elements in public project you can inform me about this by e-mail,
- * of course if you want it.
- *
- * web-site: https://github.com/ChrisMicro/GuiPittix
- */
-
