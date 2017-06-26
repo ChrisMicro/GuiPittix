e4603c939242e9b30674af6c9cb2dd6216f12a70
ChrisMicro <c.hab@gmx.net>
2017-06-26 08:42:31 +0200

    tft adapters separated, converted to library

---
+++ TFT_Adapter.h
@@ -0,0 +1,7 @@
+#ifdef TFT_Adapter_STM32F746_Discovery
+   #include "TFT_Adapter_STM32F746_Discovery.h"
+#elif defined TFT_Adapter_ILI9341_BluePill
+   #include "TFT_Adapter_ILI9341_BluePill.h"
+#elif defined TFT_Adapter_SPFD5408_UNO
+   #include "TFT_Adapter_SPFD5408_UNO.h"
+#endif
